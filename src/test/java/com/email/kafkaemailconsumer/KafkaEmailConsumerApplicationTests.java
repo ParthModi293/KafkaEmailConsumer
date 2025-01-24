c7c59d9f7ff09b08e392abf7eb008270971235f9
mtfive001 <mt5fdev1@outlook.com>
2025-01-24 17:57:05 +0530

    Initial commit

---
+++ src/test/java/com/email/kafkaemailconsumer/KafkaEmailConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.kafkaemailconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEmailConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
